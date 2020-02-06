3c7b85fa4bf6f1daddf1cf047a1000c7d9d13fae
Jondeus <joni.laukkonen@gmail.com>
2020-02-06 16:37:36 +0200

    Added cases module

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+$(document).ready(function() {
+  // Accordions
+  var accordion_items_bodies = $(".accordion-item__body");
+  $(accordion_items_bodies).hide();
 
+  var accordion_items = $(".accordion-item__header");
+  $(accordion_items).click(function() {
+    $(this)
+      .siblings(".accordion-item__body")
+      .slideToggle();
+    $(this).toggleClass("accodion-item__header--active");
+  });
+});
